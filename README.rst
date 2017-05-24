fd02eb955981fc93f8c659309fccba0711439690
Jonathan Bouzekri <jonathan.bouzekri@gmail.com>
2017-05-24 23:16:32 +0200

    prepare new api

---
+++ README.rst
@@ -0,0 +1,25 @@
+gmalt API
+=========
+
+gMalt API returning an altitude from a position
+
+Installation
+------------
+
+TODO
+
+Usage
+-----
+
+TODO
+
+TODO
+----
+
+* pass config file in command line argument or option
+* configure folder to find HGT file
+* implement extract elevation from hgt file in task
+* manage error in webservice
+* get coordinates from GET params
+* logging in webservice
+* update readme
--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
 # -*- coding: utf-8 -*-
 #
-# (c) 2016 Jonathan Bouzekri
+# (c) 2017 Jonathan Bouzekri
 #
 # This file is part of the gMalt application
 #
-# MIT License : https://raw.githubusercontent.com/gmalt/fs-service/master/LICENSE
+# MIT License : https://raw.githubusercontent.com/gmalt/api/master/LICENSE
 
 import os
 from setuptools import setup, find_packages
 
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return open(os.path.join(os.path.dirname(__file__), fname)).read().strip()
 
 
 setup(
-    name = "gmaltfileservice",
-    version = read('VERSION'),
-    author = "Jonathan Bouzekri",
-    author_email = "jonathan.bouzekri@gmail.com",
-    description = ("gMalt service using data from HGT files directly (not recommended for production)"),
-    license = "MIT",
-    keywords = "gis elevation altitude api webservice hgt",
-    url = "http://github.com/gmalt/fileservice",
+    name="gmaltapi",
+    version=read('VERSION'),
+    author="Jonathan Bouzekri",
+    author_email="jonathan.bouzekri@gmail.com",
+    description="gMalt server returning the altitude of a position",
+    license="MIT",
+    keywords="gis elevation altitude api webservice hgt",
+    url="http://github.com/gmalt/fileservice",
     packages=find_packages(),
-    long_description=read('README.md'),
+    long_description=read('README.rst'),
     install_requires=(
         'celery', 'redis', 'weedi', 'gevent'
     ),
@@ -42,11 +42,11 @@ setup(
     ],
     entry_points='''
         [console_scripts]
-        gmalt-file-server = gmaltfileservice.app:run_server
-        gmalt-file-worker = gmaltfileservice.app:run_worker
+        gmalt-api-server = gmaltapi.app:run_server
+        gmalt-api-worker = gmaltapi.app:run_worker
 
         [services]
-        celery = gmaltfileservice.task:GmaltCelery
-        server = gmaltfileservice.server:GmaltServer
+        celery = gmaltapi.task:GmaltCelery
+        server = gmaltapi.server:GmaltServer
     '''
 )
